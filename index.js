df3090a5a30e3be0a0b3ce41ca7f3b0748ffc9a4
Harshdeep Singh Batra <batraharshdeep2003@gmail.com>
2024-03-03 13:19:42 +0530

    Add jwt auth, user route to get logged user data, and logout route

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const express = require('express')
 const app = express()
 const db = require('./config/db')
 const port = process.env.PORT || 9000
-const { addUser, loginUser } = require('./controllers/userController')
-const { checkUserExists, checkPassword } = require('./middlewares/userMiddleware')
+const { addUser, loginUser, getLoggedUserData, logout } = require('./controllers/userController')
+const { authenticateUser, checkUserExists, checkPassword } = require('./middlewares/userMiddleware')
+const cookies = require("cookie-parser");
 
 app.use(express.json())
+app.use(cookies());
 
 const cors = require('cors')
 app.use(cors())
@@ -15,6 +17,10 @@ app.use(cors())
 app.post('/signup', checkUserExists, addUser)
 
 app.post('/login', checkUserExists, checkPassword, loginUser)
+
+app.get("/user", authenticateUser, getLoggedUserData)
+
+app.delete("/logout", authenticateUser, logout);
 
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
