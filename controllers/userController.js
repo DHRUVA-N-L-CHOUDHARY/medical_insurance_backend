7e320ca85ba1fd1baad0d0644a5c4c4043993f28
DhruvaBullsurge <dhruva.choudhary@bullsurge.in>
2024-03-03 10:46:09 +0530

    issue resolve

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const User = require('../models/User.db')
+const User = require('../models/user.db')
 const bcrypt = require("bcrypt")
 
 const addUser = async (req, res) => {
